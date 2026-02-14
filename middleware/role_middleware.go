b1588afcc5d468e9f8a537aa3a998a610c775c2f
pklsmkmq <pklsmkmq@gmail.com>
2026-02-14 07:32:48 +0700

    push baru

--- middleware/role_middleware.go
+++ middleware/role_middleware.go
@@ -4,7 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func AdminOnly() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if c.GetString("role") != "admin" {
+		if c.GetString("role") != "" {
 			c.JSON(403, gin.H{"message": "Admin only"})
 			c.Abort()
 			return
