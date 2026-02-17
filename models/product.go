67309cc577eb0cc99768842737876747a9aad7ba
pklsmkmq <pklsmkmq@gmail.com>
2026-02-17 13:38:47 +0700

    fix

--- models/product.go
+++ models/product.go
@@ -1,13 +1,13 @@
 package models
 
 type Product struct {
-	ID          int     `json:"id"`
+	ID          int     `json:"id,omitempty"`
 	Name        string  `json:"name"`
 	Category    string  `json:"category"`
 	Price       int     `json:"price"`
 	ImageURL    string  `json:"image_url"`
 	Description string  `json:"description"`
 	Rating      float64 `json:"rating"`
-	CreatedAt   string  `json:"created_at"`
-	UpdatedAt   string  `json:"updated_at"`
+	CreatedAt   *string `json:"created_at,omitempty"`
+	UpdatedAt   *string `json:"updated_at,omitempty"`
 }
